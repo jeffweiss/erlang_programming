4daa263d09f773ee843aec1cb8c6ff9b14cad8c3
Jeff Weiss <jeff.weiss@puppetlabs.com>
2013-03-20 01:19:00 -0700

    solution to exercise 2-3

---
+++ ch02/boolean.erl
@@ -0,0 +1,10 @@
+-module (boolean).
+-export ([b_not/1,b_and/2,b_nand/2]).
+
+b_not(false) -> true;
+b_not(true)  -> false.
+
+b_and(true, true) -> true;
+b_and(_,_) -> false.
+
+b_nand(A,B) -> b_not(b_and(A,B)).
