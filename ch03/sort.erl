e652f4089dde647c1c9e8491823fbc115fbeb6be
Jeff Weiss <jeff.weiss@puppetlabs.com>
2013-03-21 01:16:18 -0700

    exercise 3.7: mergesort

--- ch03/sort.erl
+++ ch03/sort.erl
@@ -9,5 +9,19 @@ quick([Pivot|Tail]) ->
 	{Below, Above} = lists:partition(fun(A) -> A < Pivot end, Tail),
 	lists:append(quick(Below),[Pivot|quick(Above)]).
 	
+merge([]) -> [];
+merge([H|[]]) -> [H];
 merge(List) ->
-	List.
+	Len = length(List),
+	Half = Len div 2,
+	Left = lists:sublist(List,Half),
+	Right = lists:sublist(List,Half+1, Len), % it's fine that Half + 1 + Len is greater than total length
+	merge(merge(Left),merge(Right)).
+merge([], Right) ->
+	merge(Right);
+merge(Left, []) ->
+	merge(Left);
+merge([LHead|LTail],[RHead|RTail]) when LHead < RHead ->
+	[LHead | merge(LTail, [RHead|RTail])];
+merge([LHead|LTail],[RHead|RTail]) ->
+	[RHead | merge([LHead|LTail], RTail)].
