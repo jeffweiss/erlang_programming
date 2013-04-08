6bdb484bf140975215c0b2b305d8fa43ab156947
Jeff Weiss <jeff.weiss@puppetlabs.com>
2013-04-07 21:57:31 -0700

    exercise 6-1: linked ping-pong echo

---
+++ ch06/echo.erl
@@ -0,0 +1,26 @@
+-module (echo).
+-export ([start/0, print/1, stop/0, server/0]).
+
+start() ->
+	register(echo, spawn_link(echo, server, [])).
+	
+print(Term) ->
+	echo ! {self(), {message, Term}},
+	receive
+		{response, Msg} ->
+			io:format("~w~n", [Msg]);
+		_Other ->
+			{error, unknown_message}
+	end.
+	
+stop() ->
+	exit(self(), kill).
+	
+server() ->
+	receive
+		{Pid, {message, Msg}} ->
+			Pid ! {response, Msg},
+			server();
+		{Pid, stop} ->
+			Pid ! ok
+	end.
